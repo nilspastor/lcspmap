9ffc2883bb1898fc4d9ddeefa35b779225da3dcd
nilspastor <nilspastor@gmail.com>
2017-10-28 13:48:34 +0200

    Update conf.php
    with comments

--- conf.php
+++ conf.php
@@ -1,5 +1,7 @@
 <?php
 
-$db = new PDO('mysql:host=localhost_or_adress;dbname=my_db_name', 'my_db_login', 'my_db_pass');
 
+// Connexion à la BDD
+$db = new PDO('mysql:host=localhost_or_adress;dbname=my_db_name', 'my_db_login', 'my_db_pass');
+// ERRORMODE
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
